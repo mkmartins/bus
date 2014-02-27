24345b58a6bfdf13ce9fea32c66e486d1050ec08
DPL-Iota <dpl-iota@DPL-Iotas-iMac.local>
2014-02-27 11:48:40 -0700

    Adds boolean field to ingredients

--- db/migrate/20140222012423_create_ingredients.rb
+++ db/migrate/20140222012423_create_ingredients.rb
@@ -2,6 +2,7 @@ class CreateIngredients < ActiveRecord::Migration
   def change
     create_table :ingredients do |t|
       t.string :name
+      t.boolean :standard
       t.integer :dish_id
       t.integer :side_dish_id
 
