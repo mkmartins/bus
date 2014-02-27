42d2f9ee03a75094d210550dbfebaea46247d38d
DPL-Iota <dpl-iota@DPL-Iotas-iMac.local>
2014-02-27 12:15:54 -0700

    Adds price field to ingredient form

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,6 +1,6 @@
 class Ingredient < ActiveRecord::Base
   belongs_to :dish
   belongs_to :side_dish
-  attr_accessible :name, :standard
+  attr_accessible :name, :standard, :price
 
 end
--- db/migrate/20140222012423_create_ingredients.rb
+++ db/migrate/20140222012423_create_ingredients.rb
@@ -3,6 +3,7 @@ class CreateIngredients < ActiveRecord::Migration
     create_table :ingredients do |t|
       t.string :name
       t.boolean :standard
+      t.decimal :price
       t.integer :dish_id
       t.integer :side_dish_id
 
