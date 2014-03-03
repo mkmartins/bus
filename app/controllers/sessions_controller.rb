0016fd10d07630548ff5d56b330c2da0250b4c8b
jjrockstar14 <jillairei@gmail.com>
2014-03-03 12:50:26 -0700

    Fixes sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,9 @@
 class SessionsController < ApplicationController
+  
   def create 
     user = User.from_omniauth(env["omniauth.auth"])
     session[:user_id] = user.user_id
     redirect_to root_url
+  end
+  
 end
